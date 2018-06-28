538eac041faa3496558861a3e82920bb4bc7b502
Melvynkoh94 <31084923+Melvynkoh94@users.noreply.github.com>
2018-06-28 11:23:02 +0800

    2-Way Currency Converter
    Removed the dollar symbol and implemented 2-way conversion. Clears the other editText when entering current EditText

--- app/src/main/java/com/example/user/bangcock/BangcockActivity.java
+++ app/src/main/java/com/example/user/bangcock/BangcockActivity.java
@@ -2,10 +2,14 @@ package com.example.user.bangcock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.Locale;
 
 public class BangcockActivity extends AppCompatActivity {
 
@@ -15,6 +19,13 @@ public class BangcockActivity extends AppCompatActivity {
     private static final double THB2SGD = 0.041;
     private static final double SGD2THB = 24.24;
     private double SGDtext, THBtext;
+    final NumberFormat currency = NumberFormat.getCurrencyInstance();
+
+    final NumberFormat currencyFormatter = NumberFormat.getInstance( Locale.US );
+
+
+
+    DecimalFormat formatter = new DecimalFormat("#,##0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +36,44 @@ public class BangcockActivity extends AppCompatActivity {
         EDITTEXT_SGD = (EditText)findViewById(R.id.editText_SGD);
         EDITTEXT_BKK = (EditText)findViewById(R.id.editText_BKK);
 
-        //NumberFormat currency = NumberFormat.getCurrencyInstance();
-        //myTextView.setText(currency.format(dollars));
 
 
         btn_CONVERT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SGDtext = Double.parseDouble(EDITTEXT_SGD.getText().toString());
-                THBtext = SGDtext * SGD2THB;
-                EDITTEXT_SGD.setText(Double.toString(SGDtext));
-                EDITTEXT_BKK.setText(Double.toString(THBtext));
-                //mValueOne = Float.parseFloat(EDITTEXT_SGD.getText() + "");
-                //mSubtract = true;
-                //crunchifyEditText.setText(null);
+                if (TextUtils.isEmpty(EDITTEXT_BKK.getText())) {
+                    SGDtext = Double.parseDouble(EDITTEXT_SGD.getText().toString());
+                    THBtext = SGDtext * SGD2THB;
+                    EDITTEXT_SGD.setText(currencyFormatter.format(SGDtext) + " SGD");
+                    EDITTEXT_BKK.setText(currencyFormatter.format(THBtext) + " BUTTS");
+                }
+
+                if (TextUtils.isEmpty(EDITTEXT_SGD.getText())) {
+                    THBtext = Double.parseDouble(EDITTEXT_BKK.getText().toString());
+                    SGDtext = THBtext * THB2SGD;
+                    EDITTEXT_SGD.setText(currencyFormatter.format(SGDtext) + " SGD");
+                    EDITTEXT_BKK.setText(currencyFormatter.format(THBtext) + " BUTTS");
+                }
+
+                //EDITTEXT_SGD.setText(Double.toString(SGDtext));
+                //EDITTEXT_BKK.setText(Double.toString(THBtext));
+            }
+        });
+
+        EDITTEXT_SGD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EDITTEXT_SGD.getText().clear();
+                EDITTEXT_BKK.getText().clear();
+
+            }
+        });
+
+        EDITTEXT_BKK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EDITTEXT_BKK.getText().clear();
+                EDITTEXT_SGD.getText().clear();
             }
         });
     }
